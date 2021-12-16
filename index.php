93d0ac98900e1de1d43463614f47626e8c77aef4
DanyMencu <dany69men@gmail.com>
2021-12-16 18:47:50 +0100

    Exercise improvement

--- index.php
+++ index.php
@@ -1,32 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad Words</title>
 </head>
+
 <body>
-    
-    <?php 
-        $paragraph = 'Lorem ipsum dolor sit amet, consectetur adip';
-        $word = $_GET['word'];
 
-        echo $paragraph;
+    <?php
+    $paragraph = 'Lorem ipsum dolor sit amet, consectetur adip';
+    $word = $_GET['word'];
 
-        echo '<br>';
+    echo $paragraph;
 
-        echo 'LENGHT:' . strlen($paragraph);
+    echo '<br>';
 
+    echo 'LENGHT:' . strlen($paragraph);
+
+    echo '<br>';
+    echo '<br>';
+    ?>
+
+    <form method="get" action="">
+        <input type="text" id="word" name="word" placeholder="Che parola censuriamo oggi?" />
+        <input type="submit" />
+    </form>
+
+    <?php
         echo '<br>';
         echo '<br>';
 
         echo str_replace($word, '***', $paragraph);
-    
+
         echo '<br>';
 
-        echo 'LENGHT:'. strlen($paragraph);
+        echo 'LENGHT:' . strlen($paragraph);
     ?>
 
 </body>
+
 </html>
